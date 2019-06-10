32e8556ec458aa8ea5f61a7044dfd88f0c377374
BabyNoCry <1633007250@qq.com>
2019-06-10 22:35:44 +0800

    ·

---
+++ vector/vector/vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	string s1;
+	s1.push_back();
+	
+	cout<<
+	system("pasue");
+	return 0;
+}
